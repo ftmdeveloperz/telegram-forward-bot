2aec6516c5d850648c26c2b580f409bc706f0980
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-03 11:56:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@
 
 import json
 import os
-
 import sys
 import telepot
 import time
 from telepot.loop import MessageLoop
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
 
 def save_status(obj):
     with open('chats.json', 'w') as f:
@@ -50,21 +51,16 @@ else:
 
 def is_allowed(msg):
     if msg['chat']['type'] == 'channel':
-        return True #all channel admins are allowed to use the bot (channels don't have sender info)
+        return True  # all channel admins are allowed to use the bot (channels don't have sender info)
     return 'from' in msg and msg['from']['id'] in allowed
 
 def handle(msg):
     print("Message: " + str(msg))
-    # Add person as allowed
     content_type, chat_type, chat_id = telepot.glance(msg)
-    txt = ""
-    if 'text' in msg:
-        txt = txt + msg['text']
-    elif 'caption' in msg:
-        txt = txt + msg['caption']
-    # Addme and rmme only valid on groups and personal chats.
+    txt = msg.get('text', msg.get('caption', ''))
+
     if msg['chat']['type'] != 'channel':
-        if "/addme" == txt.strip()[:6]:
+        if txt.startswith("/addme"):
             if msg['chat']['type'] != 'private':
                 bot.sendMessage(chat_id, "This command is meant to be used only on personal chats.")
             else:
@@ -72,83 +68,75 @@ def handle(msg):
                 if used_password == PASSWORD:
                     allowed.add(msg['from']['id'])
                     save_allowed(allowed)
-                    bot.sendMessage(chat_id, msg['from']['first_name'] + ", you have been registered " +
-                                    "as an authorized user of this bot.")
+                    bot.sendMessage(chat_id, f"{msg['from']['first_name']}, you have been registered as an authorized user of this bot.")
                 else:
                     bot.sendMessage(chat_id, "Wrong password.")
-        if "/rmme" == txt.strip()[:5]:
+        elif txt.startswith("/rmme"):
             allowed.remove(msg['from']['id'])
             save_allowed(allowed)
             bot.sendMessage(chat_id, "Your permission for using the bot was removed successfully.")
+    
     if is_allowed(msg):
-        if txt != "":
-            if "/add " == txt[:5]:
-                txt_split = txt.strip().split(" ")
-                if len(txt_split) == 2 and "#" == txt_split[1][0]:
-                    tag = txt_split[1].lower()
-                    name = ""
-                    if msg['chat']['type'] == "private":
-                        name = name + "Personal chat with " + msg['chat']['first_name'] + ((" " + msg['chat']['last_name']) if 'last_name' in msg['chat'] else "")
-                    else:
-                        name = msg['chat']['title']
-                    chats[tag] = {'id': chat_id, 'name': name}
-                    bot.sendMessage(chat_id, name + " added with tag " + tag)
+        if txt.startswith("/add "):
+            txt_split = txt.strip().split(" ")
+            if len(txt_split) == 2 and txt_split[1][0] == "#":
+                tag = txt_split[1].lower()
+                name = msg['chat'].get('title', f"Personal chat with {msg['chat'].get('first_name', '')}")
+                chats[tag] = {'id': chat_id, 'name': name}
+                bot.sendMessage(chat_id, f"{name} added with tag {tag}")
+                save_status(chats)
+            else:
+                bot.sendMessage(chat_id, "Incorrect format. It should be _/add #{tag}_", parse_mode="Markdown")
+        elif txt.startswith("/rm "):
+            txt_split = txt.strip().split(" ")
+            if len(txt_split) == 2 and txt_split[1][0] == "#":
+                tag = txt_split[1].lower()
+                if tag in chats and chats[tag]['id'] == chat_id:
+                    del chats[tag]
+                    bot.sendMessage(chat_id, f"Tag {tag} deleted from taglist.")
                     save_status(chats)
                 else:
-                    bot.sendMessage(chat_id, "Incorrect format. It should be _/add #{tag}_", parse_mode="Markdown")
-            elif "/rm " == txt[:4]:
-                txt_split = txt.strip().split(" ")
-                if len(txt_split) == 2 and "#" == txt_split[1][0]:
-                    tag = txt_split[1].lower()
+                    bot.sendMessage(chat_id, "Tag doesn't exist on TagList or you can't delete it from a different chat.")
+        elif txt.startswith("/taglist"):
+            response = "<b>TagList</b>"
+            for tag, chat in sorted(chats.items()):
+                response += f"\n<b>{tag}</b>: <i>{chat['name']}</i>"
+            bot.sendMessage(chat_id, response, parse_mode="HTML")
+        elif txt.startswith("#"):
+            tags = [t.lower() for t in txt.strip().split() if t.startswith("#")]
+            if tags:
+                for tag in tags:
                     if tag in chats:
-                        if chats[tag]['id'] == chat_id:
-                            del chats[tag]
-                            bot.sendMessage(chat_id, "Tag "+tag+" deleted from taglist.")
-                            save_status(chats)
-                            return
-                        else:
-                            bot.sendMessage(chat_id, "You can't delete a chat's tag from a different chat.")
+                        bot.forwardMessage(chats[tag]['id'], chat_id, msg['message_id'])
+                        if 'reply_to_message' in msg:
+                            bot.forwardMessage(chats[tag]['id'], chat_id, msg['reply_to_message']['message_id'])
                     else:
-                        bot.sendMessage(chat_id, "Tag doesn't exist on TagList")
-                else:
-                    bot.sendMessage(chat_id, "Incorrect format. It should be _/rm #{tag}_", parse_mode="Markdown")
-
-            elif "/taglist" ==  txt.strip()[:8]:
-                tags_names = []
-                for tag, chat in chats.items():
-                    tags_names.append( (tag, chat['name']))
-                response = "<b>TagList</b>"
-                for (tag, name) in sorted(tags_names):
-                    response = response + "\n<b>" + tag + "</b>: <i>" + name + "</i>"
-                bot.sendMessage(chat_id, response, parse_mode="HTML")
-            elif "#" == txt[0]:
-                txt_split =txt.strip().split(" ")
-                i = 0
-                tags = []
-                while i < len(txt_split) and txt_split[i][0] == "#":
-                    tags.append(txt_split[i].lower())
-                    i+=1
-                if i != len(txt_split) or 'reply_to_message' in msg:
-                    approved = []
-                    rejected = []
-                    for tag in tags:
-                        if tag in chats:
-                            if chats[tag]['id'] != chat_id:
-                                approved.append(chats[tag]['name'])
-                                bot.forwardMessage(chats[tag]['id'], chat_id, msg['message_id'])
-                                if 'reply_to_message' in msg:
-                                    bot.forwardMessage(chats[tag]['id'], chat_id, msg['reply_to_message']['message_id'])
-                        else:
-                            rejected.append(tag)
-                    if len(rejected) > 0:
-                        bot.sendMessage(chat_id, "Failed to send messages to tags <i>" + ", ".join(rejected) + "</i>", parse_mode="HTML")
-                else:
-                    bot.sendMessage(chat_id, "Failed to send a message only with tags which is not a reply to another message")
+                        bot.sendMessage(chat_id, f"Failed to send messages to tag {tag}", parse_mode="HTML")
 
 bot = telepot.Bot(TOKEN)
 
 MessageLoop(bot, handle).run_as_thread()
 print('Listening ...')
-# Keep the program running.
-while 1:
+
+# Define a simple HTTP server to keep the bot running and listen on port 8080
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_http_server():
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, SimpleHandler)
+    print("Starting HTTP server on port 8080...")
+    httpd.serve_forever()
+
+# Run HTTP server in a separate thread
+http_thread = threading.Thread(target=run_http_server)
+http_thread.daemon = True
+http_thread.start()
+
+# Keep the bot running
+while True:
     time.sleep(10)
